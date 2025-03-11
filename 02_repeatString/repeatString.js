2866da3e40903be849460325918b62be6babd6b2
Davide Gamba <gamba.davide1996@gmail.com>
2025-03-11 19:46:14 +0100

    Exercise solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+
+  if (num < 0 ) return 'ERROR';
+
+  let result = ''
+  for(i = 0; i < num; i++){
+    result = result.concat(string)
+  }
+
+  return result 
 
 };
+
+repeatString("ciao",2)
 
 // Do not edit below this line
 module.exports = repeatString;
