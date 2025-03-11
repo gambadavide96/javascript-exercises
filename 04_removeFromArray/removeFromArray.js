bc98ef722b86dc427eddc07cd1249090b7ff1c8a
Davide Gamba <gamba.davide1996@gmail.com>
2025-03-11 22:06:21 +0100

    Setting parameters

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayToManipulate, ...parameters) {
+
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
