ffa94192d77c9c2d9083b2e31848440a5befff66
Davide Gamba <gamba.davide1996@gmail.com>
2025-03-12 19:42:01 +0100

    Exercise solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,15 @@
+
 const removeFromArray = function(arrayToManipulate, ...parameters) {
 
-};
+  parameters.forEach(
+    (param) => {
+      arrayToManipulate = arrayToManipulate.filter(value => value !== param);
+    }
+  )
 
+  return arrayToManipulate;
+
+};
 
 // Do not edit below this line
 module.exports = removeFromArray;
