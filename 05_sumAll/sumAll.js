7ce265e5218bcf675fa6f862c39bf51a6aba6772
Davide Gamba <gamba.davide1996@gmail.com>
2025-03-13 10:46:18 +0100

    Exercise solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function(first, last) {
+
+  if( !(Number.isInteger(first) && Number.isInteger(last)) || (first || last) < 0)
+    return "ERROR"
+
+  if (first > last)
+  [first, last] = [last, first]
+
+  let sum = 0
+  for(let i = first; i <= last; i++){
+    sum += i
+  }
+
+  return sum
+
 
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
