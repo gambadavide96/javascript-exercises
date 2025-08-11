39de5a691118ed223853be3016b6fed21d3aaf7b
Davide Gamba <gamba.davide1996@gmail.com>
2025-08-11 23:12:07 +0200

    solving exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  // espr. regolare, sostituisce con niente tutti i caratteri che non sono lettere o numeri
+  str = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+  const n = str.length;
+  for(let i = 0; i < Math.floor(n / 2) ; i++) {
+    if(str[i] !== str[n - 1 - i])
+      return false;
+  }
+  return true;
 };
 
 // Do not edit below this line
