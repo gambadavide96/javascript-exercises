f1160097b1c5fd94a0420f0580d54bab7d12894d
Davide Gamba <gamba.davide1996@gmail.com>
2025-08-12 19:35:40 +0200

    Solving exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,4 +1,19 @@
-const findTheOldest = function() {
+
+const getAge = (birth,death) => {
+  if(death == undefined)
+    death = new Date().getFullYear();
+
+  return death - birth;
+}
+
+
+const findTheOldest = function(people) {
+
+  return people.reduce((oldest,currentPerson) => {
+    const oldestAge = getAge(oldest.yearOfBirth,oldest.yearOfDeath);
+    const currentPersonAge = getAge(currentPerson.yearOfBirth,currentPerson.yearOfDeath);
+    return oldestAge >= currentPersonAge ? oldest : currentPerson;
+  });
 
 };
 
