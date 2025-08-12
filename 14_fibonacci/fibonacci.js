bbabdd4738b6a536097b267396e4a23af8f12140
Davide Gamba <gamba.davide1996@gmail.com>
2025-08-12 18:04:30 +0200

    Solving exercise 14 fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(pos) {
+  if (typeof pos == 'string')
+    pos = parseInt(pos);
+
+  if(pos < 0)
+    return 'OOPS';
+
+  if(pos == 0)
+    return 0;
+
+  let prev2 = 0; //posizione 0
+  let prev1 = 1; //posizione 1
+  for(let i = 1; i < pos; i++){
+    let tail = prev1 + prev2;
+    prev2 = prev1
+    prev1 = tail
+  }
+
+  return prev1;
 
 };
 
